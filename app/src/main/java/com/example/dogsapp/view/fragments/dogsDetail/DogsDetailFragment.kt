8c1c4ee1ac0fd74bfe7b8054aec3d744ac91e202
bezrukovq <bezrukovq@yandex.ru>
2019-03-26 12:50:09 +0300

    transition fixed to normal

--- app/src/main/java/com/example/dogsapp/view/fragments/dogsDetail/DogsDetailFragment.kt
+++ app/src/main/java/com/example/dogsapp/view/fragments/dogsDetail/DogsDetailFragment.kt
@@ -1,24 +1,22 @@
 package com.example.dogsapp.view.fragments.dogsDetail
 
 import android.os.Build
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.transition.TransitionInflater
 import com.example.dogsapp.R
 import com.example.dogsapp.di.di
 import com.example.dogsapp.vm.BaseViewModelFactory
-
 import com.example.dogsapp.vm.DogsDetailViewModel
-import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.dogs_detail_fragment.*
 import org.kodein.di.generic.instance
-import java.lang.Exception
 
 class DogsDetailFragment : Fragment() {
 
@@ -27,14 +25,15 @@ class DogsDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.activity?.let { ActivityCompat.postponeEnterTransition(it) }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setSharedElementEnterTransition(TransitionInflater.from(getContext())
+                .inflateTransition(android.R.transition.move));
+        }
         /*
         val transition = TransitionInflater.from(context)
             .inflateTransition(R.transition.shared_element_transition)
         sharedElementEnterTransition = transition*/
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            sharedElementEnterTransition =
-                TransitionInflater.from(context).inflateTransition(android.R.transition.slide_left)
-        }
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(DogsDetailViewModel::class.java)
         viewModel.showProgress.observe(this, Observer { aBoolean ->
             if (aBoolean) {
@@ -44,7 +43,7 @@ class DogsDetailFragment : Fragment() {
             }
         }
         )
-        var dogBreed = arguments?.getString("dog").toString()
+        val dogBreed = arguments?.getString("dog").toString()
         viewModel.getDogPic(dogBreed)
         viewModel.getDogBreed(dogBreed)
         viewModel.dogData.observe(this, Observer { dog -> setImg(dog.message) })
@@ -52,13 +51,16 @@ class DogsDetailFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.activity?.let { ActivityCompat.startPostponedEnterTransition(it) }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tv_breed_det.setTransitionName(arguments?.getString("trId"))
+        }
+    }
+
     private fun setImg(img: String) {
-        Picasso.get().load(img).into(iv_dog_img, object : Callback {
-            override fun onError(e: Exception?) {}
-            override fun onSuccess() {
-                hideProgress()
-            }
-        })
+        Picasso.get().load(img).into(iv_dog_img)
     }
 
     private fun setText(text: String) {
@@ -77,9 +79,10 @@ class DogsDetailFragment : Fragment() {
     }
 
     companion object {
-        fun newInstance(dog: String): DogsDetailFragment {
+        fun newInstance(dog: String, transitionId : String): DogsDetailFragment {
             val args = Bundle()
             args.putString("dog", dog)
+            args.putString("trId", transitionId)
             val fragment = DogsDetailFragment()
             fragment.arguments = args
             return fragment
